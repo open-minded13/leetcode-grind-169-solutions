a9627953ad4f7e02393087101d8139e040801bbf
Chia-Wei <52095472+open-minded13@users.noreply.github.com>
2024-04-16 22:01:37 -0700

    renamed:    Part 2/Easy 278. First Bad Version.py -> Part 1/Easy 278. First Bad Version.py 	renamed:    Part 2/Easy 383. Ransom Note.py -> Part 1/Easy 383. Ransom Note.py 	renamed:    Part 2/Easy 409. Longest Palindrome.py -> Part 1/Easy 409. Longest Palindrome.py 	renamed:    Part 2/Easy 70. Climbing Stairs.py -> Part 1/Easy 70. Climbing Stairs.py 	renamed:    Part 8/Hard 224. Basic Calculator.py -> Part 13/Hard 224. Basic Calculator.py 	renamed:    Part 13/Hard 1235. Maximum Profit in Job Scheduling.py -> Part 14/Hard 1235. Maximum Profit in Job Scheduling.py 	renamed:    Part 8/Hard 23. Merge k Sorted Lists.py -> Part 14/Hard 23. Merge k Sorted Lists.py 	renamed:    Part 8/Hard 84. Largest Rectangle in Histogram.py -> Part 14/Hard 84. Largest Rectangle in Histogram.py 	renamed:    Part 2/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray (Dynamic Programming - Memoization).cpp -> Part 3/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray (Dynamic Programming - Memoization).cpp 	renamed:    Part 2/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray (Kadane Algo).cpp -> Part 3/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray (Kadane Algo).cpp 	renamed:    Part 2/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray.py -> Part 3/Medium 53. Maximum Subarray/Medium 53. Maximum Subarray.py 	renamed:    Part 3/Medium 102. Binary Tree Level Order Traversal.py -> Part 4/Medium 102. Binary Tree Level Order Traversal.py 	renamed:    Part 3/Medium 133. Clone Graph.py -> Part 4/Medium 133. Clone Graph.py 	renamed:    Part 3/Medium 15. 3Sum.py -> Part 4/Medium 15. 3Sum.py 	renamed:    Part 3/Medium 150. Evaluate Reverse Polish Notation.py -> Part 4/Medium 150. Evaluate Reverse Polish Notation.py 	renamed:    Part 6/Medium 139. Word Break.py -> Part 5/Medium 139. Word Break.py 	renamed:    Part 4/Medium 994. Rotting Oranges.py -> Part 5/Medium 994. Rotting Oranges.py 	renamed:    Part 7/Medium 11. Container with Most Water.py -> Part 6/Medium 11. Container with Most Water.py 	deleted:    Part 6/Medium 146. LRU Cache.py 	renamed:    Part 7/Medium 17. Letter Combinations of a Phone Number.py -> Part 6/Medium 17. Letter Combinations of a Phone Number.py 	renamed:    Part 7/Medium 79. Word Search.py -> Part 6/Medium 79. Word Search.py 	deleted:    Part 7/Hard 84. Largest Rectangle in Histogram.py 	modified:   Part 7/Medium 146. LRU Cache.py 	renamed:    Part 8/Medium 230. Kth Smallest Element in a BST.py -> Part 7/Medium 230. Kth Smallest Element in a BST.py 	renamed:    Part 7/Medium 439. Find All Anagrams in a String.py -> Part 7/Medium 438. Find All Anagrams in a String.py

--- Part 7/Medium 146. LRU Cache.py
+++ Part 7/Medium 146. LRU Cache.py
@@ -1,8 +1,30 @@
 # Date of Last Practice: Jan 9, 2024 -> Feb 22, 2024
 #
-# Time Complexity: O(1) because we use hash table and doubly linked list.
+# Time Complexity: O(1), where N is the length of the string s.
+#                  - get Operation (O(1)): Checking if a key exists in the nodes dictionary
+#                    and accessing its corresponding node is an O(1) operation.
+#                  - put Operation (O(1)): Checking if a key exists in the nodes dictionary is O(1).
+#                    Adding a new node and adjusting pointers to make it the new head is O(1).
+#                    In the case of exceeding capacity, removing the tail node and
+#                    updating pointers is O(1).
+#                  - move_to_front Method (O(1)): Involves only a fixed number of pointer changes,
+#                    irrespective of the size of the cache.
 #
-# Space Complexity: O(N), where N is the capacity of the LRU cache.
+# Space Complexity: O(N), where N is the length of the string s.
+#                   The space complexity is primarily due to the dp array,
+#                   which has a size of O(n).
+#
+# Garbage Collection: In Python, when an object's reference count drops to zero
+#                     (meaning there are no references to it in the program),
+#                     it becomes eligible for garbage collection.
+#                     This means that the memory occupied by the object can be freed.
+#
+#                     self.tail = self.tail.prev
+#                     self.tail.next = None
+#
+#                     [Node A]↔[Node B]    >>   [Node A]→None    [Node B]
+#                       ↑        ↑                  ↑
+#                      head     tail           head & tail
 
 
 class Node:
@@ -15,6 +37,62 @@ class Node:
 
 
 class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.current_size = 0
+        self.head = None
+        self.tail = None
+        self.nodes = {}
+
+    def get(self, key: int) -> int:
+        if key in self.nodes:
+            self.move_to_front(key)
+            return self.nodes[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.nodes:
+            new_node = Node(key, value)
+            self.nodes[key] = new_node
+            if not self.head:
+                self.head = self.tail = new_node
+            else:
+                new_node.next = self.head
+                self.head.prev = new_node
+                self.head = new_node
+            self.current_size += 1
+        else:
+            self.nodes[key].val = value
+            self.move_to_front(key)
+
+        if self.current_size > self.capacity:
+            # Evict the least recently used node
+            evicted_key = self.tail.key
+            self.tail = self.tail.prev
+            if self.tail:
+                self.tail.next = None
+            else:
+                self.head = None
+            self.nodes.pop(evicted_key, None)
+            self.current_size -= 1
+
+    def move_to_front(self, key):
+        current_node = self.nodes[key]
+        if current_node == self.head:
+            return
+        if current_node == self.tail:
+            self.tail = current_node.prev
+        if current_node.prev:
+            current_node.prev.next = current_node.next
+        if current_node.next:
+            current_node.next.prev = current_node.prev
+        current_node.next = self.head
+        current_node.prev = None
+        self.head.prev = current_node
+        self.head = current_node
+
+
+class OtherLRUCache1:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
@@ -61,7 +139,7 @@ class LRUCache:
             self.cache_size -= 1
 
 
-class AnotherLRUCache:
+class OtherLRUCache2:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
