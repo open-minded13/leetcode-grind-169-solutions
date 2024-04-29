ff9d6865b24f064e7a15aed44a60c868717fea89
Chia-Wei <52095472+open-minded13@users.noreply.github.com>
2024-04-29 04:49:47 -0700

    new file:   Part 1/Easy 1. Two Sum/Easy 1. Two Sum.js 	new file:   Part 1/Easy 1. Two Sum/Easy 1. Two Sum.test.js

---
+++ Part 1/Easy 1. Two Sum/Easy 1. Two Sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const twoSum = (nums, target) => {
+    seen = {}
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i]
+        if (complement in seen) {
+            return [seen[complement], i]
+        }
+        seen[nums[i]] = i
+    }
+    return [-1, -1]
+}
+
+module.exports = twoSum;
